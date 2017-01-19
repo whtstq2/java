036b35782908c289634101a8af200bd099046ac2
206-023\206-023 <s2cayas2@naver.com>
2017-01-19 17:03:12 +0900

    새로고침

--- Test/src/test.java
+++ Test/src/test.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 
 public class test {
+	
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		Scanner input= new Scanner(System. in);
+		
+		int num;
+	
+		
+		System.out.println("======������ ���α׷�======");
+		System.out.println("���Ͻô� �������� �Է��ϼ��� (�Է¹��� : 2~9)");
+		System.out.print("���ϴ� ������ :");
+		
+		num = input.nextInt();
+		
+		System.out.println("====================");
+		System.out.println("������"+ num + "���� ����մϴ�.");
+		
+		int l=0;
+		while (l<9) {
+			l++;
+			System.out.println(num + "*" + l + "=" + (num*l));
+		}
+				
+				input.close();
+				}
+		
+		
+		
 	}
-}
+
