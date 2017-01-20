2105ba4e256a6a52d7b40571b5037917cf013899
206-023\206-023 <s2cayas2@naver.com>
2017-01-20 09:32:08 +0900

    어제 덜올린거

--- ForTest/src/Arrays.java
+++ ForTest/src/Arrays.java
@@ -1,7 +1,8 @@
 
 public class Arrays {
 
-	public static void main(String[] args) {
+	public static 
+	void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		String[] names = {"tom", "jane", "alisia" };
